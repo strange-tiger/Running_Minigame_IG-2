76c160fac6de358f26c8e182f657dd221f4f27e8
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-09-22 17:29:19 +0900

    [Feat] Gold Coins 에셋 적용 & Coin.prefab 구현 시작
    지속적인 회전
    충돌 시 GetCoin 이벤트 Invoke
    구현
    
    이슈
    에셋 크기가 너~무 작아, 스케일 100배

---
+++ Assets/Coin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coin : MonoBehaviour
+{
+    public UnityEvent GetCoin;
+    
+    [SerializeField]
+    private float rotationSpeed = 60f;
+    private int playerLayer;
+
+    private void Awake()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == playerLayer)
+        {
+            GetCoin.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+}
