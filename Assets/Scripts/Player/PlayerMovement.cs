a8e32c9720c22e66813263f96af6ffd4a083fe4d
csy-59 <csy010509@naver.com>
2022-09-23 09:55:18 +0900

    Revert "[Feat] 플렛폼 프리팹 4종 추가"
    This reverts commit ff523db74384f059d22fb4a8630146c2333712ae.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float[] _moveXPositions = { -2.5f, 0, 2.5f };
     [SerializeField] private float _moveSpeed = 2.5f;
     private int _currentMovePosition = 1;
-    [SerializeField] private bool _isMoving = false;
+    private bool _isMoving = false;
 
     [Header("Jump")]
     [SerializeField] private float _jumpHeight = 2f;
     [SerializeField] private float _jumpSpeed = 2f;
-    [SerializeField] private bool _isJumping = false;
+    private bool _isJumping = false;
 
     // 기본 필요 component
     private Rigidbody _rigidbody;
