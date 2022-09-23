a7dc39997cfef28d2a9b8cbc12fbb04675e7281c
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-09-23 18:14:13 +0900

    [Feat] WaitingRoom UI 기능 구현
    WaitingRoom UI 기능 랭킹 제외 구현
    WaitingUI.cs

--- Assets/Scripts/UIScirpts/WaitingUI.cs
+++ Assets/Scripts/UIScirpts/WaitingUI.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaitingUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum EWaitingUIIndex
     {
-        
+        Quit,
+        LogOut,
+        Max
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject LogOutUI;
+    public GameObject QuitUI;
+
+    public Button StartBtn;
+    public Button QuitBtn;
+    public Button LogOutBtn;
+
+    private void OnEnable()
     {
-        
+        StartBtn.onClick.AddListener(StartGame);
+        QuitBtn.onClick.AddListener(LoadQuit);
+        LogOutBtn.onClick.AddListener(LoadLogOut);
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void LoadUI(EWaitingUIIndex ui)
+    {
+        LogOutUI.SetActive(false);
+        QuitUI.SetActive(false);
+
+        switch (ui)
+        {
+            case EWaitingUIIndex.Quit:
+                QuitUI.SetActive(true);
+                break;
+            case EWaitingUIIndex.LogOut:
+                LogOutUI.SetActive(true);
+                break;
+            default:
+                Debug.Assert(ui >= EWaitingUIIndex.Max, "Error: No UI Exists");
+                break;
+        }
+    }
+
+    public void LoadQuit() => LoadUI(EWaitingUIIndex.Quit);
+    public void LoadLogOut() => LoadUI(EWaitingUIIndex.LogOut);
+
+    private void OnDisable()
+    {
+        StartBtn.onClick.RemoveListener(StartGame);
+        QuitBtn.onClick.RemoveListener(LoadQuit);
+        LogOutBtn.onClick.RemoveListener(LoadLogOut);
     }
 }
