ed465b141a374af2a53133615fcce8e433782d8f
strange-tiger <jinolee9525@gmail.com>
2022-09-27 13:57:29 +0900

    [Refac] 리팩토링
    - 변수명 컨벤션에 맞게 변경
    - Assert 조건 정리

--- Assets/Scripts/Util/GetRanking.cs
+++ Assets/Scripts/Util/GetRanking.cs
@@ -6,7 +6,7 @@ using MySql.Data.MySqlClient;
 public class GetRanking
 {
     private string _userId;
-    private bool hasUserId = false;
+    private bool _hasUserId = false;
 
     // 최고 기록
     private int _highScore;
@@ -26,7 +26,7 @@ public class GetRanking
 
     private string GetPlayerId()
     {
-        if (hasUserId)
+        if (_hasUserId)
         {
             return _userId;
         }
@@ -36,7 +36,7 @@ public class GetRanking
         if (PlayerPrefs.HasKey("ID"))
         {
             _userId = PlayerPrefs.GetString("ID");
-            hasUserId = true;
+            _hasUserId = true;
         }
 
         return _userId;
@@ -63,7 +63,7 @@ public class GetRanking
 
     public void SetNewHighScore(int newHighScore)
     {
-        Debug.Assert(newHighScore < HighScore,
+        Debug.Assert(newHighScore > HighScore,
             $"새로운 점수 {newHighScore} 보다 기존 점수 {HighScore}가 더 높음");
 
         string updateScoreString = _updateScoreText.text + $"{newHighScore} WHERE ID = '{GetPlayerId()}'";
@@ -85,7 +85,7 @@ public class GetRanking
             MySqlCommand _selectMyHighScore = new MySqlCommand(_selectScoreString, _sqlConnection);
             MySqlDataReader _readHighScore = _selectMyHighScore.ExecuteReader();
 
-            Debug.Assert(_readHighScore.Read() == false, "기록 없음");
+            Debug.Assert(_readHighScore.Read() == true, "기록 없음");
 
             if (_readHighScore.Read() != false)
             {
