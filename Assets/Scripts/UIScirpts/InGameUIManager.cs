e99815ee7285230ab253cdb48a50285068129d32
strange-tiger <jinolee9525@gmail.com>
2022-09-27 11:04:57 +0900

    [Refac] InGameUIManager 코드 리팩토링
    - 변수명 컨벤션에 맞춰 수정

--- Assets/Scripts/UIScirpts/InGameUIManager.cs
+++ Assets/Scripts/UIScirpts/InGameUIManager.cs
@@ -7,25 +7,25 @@ using UnityEngine.SceneManagement;
 public class InGameUIManager : MonoBehaviour
 {
     [Header("InGamePanel")]
-    [SerializeField] private GameObject inGamePanel;
-    [SerializeField] private TextMeshProUGUI inGameScoreText;
+    [SerializeField] private GameObject _inGamePanel;
+    [SerializeField] private TextMeshProUGUI _inGameScoreText;
 
     [Header("MenuPanel")]
-    [SerializeField] private GameObject MenuPanel;
+    [SerializeField] private GameObject _menuPanel;
 
     [Header("GameOverPanel")]
-    [SerializeField] private GameObject GameOverPanel;
-    [SerializeField] private TextMeshProUGUI gameOverScoreText;
-    [SerializeField] private TextMeshProUGUI highScoreText;
-    [SerializeField] private GameObject newHighScoreText;
+    [SerializeField] private GameObject _gameOverPanel;
+    [SerializeField] private TextMeshProUGUI _gameOverScoreText;
+    [SerializeField] private TextMeshProUGUI _highScoreText;
+    [SerializeField] private GameObject _newHighScoreText;
 
     private void Awake()
     {
-        inGamePanel.SetActive(true);
+        _inGamePanel.SetActive(true);
 
-        MenuPanel.SetActive(false);
-        GameOverPanel.SetActive(false);
-        newHighScoreText.SetActive(false);
+        _menuPanel.SetActive(false);
+        _gameOverPanel.SetActive(false);
+        _newHighScoreText.SetActive(false);
 
         Time.timeScale = 1f;
     }
@@ -37,29 +37,27 @@ public class InGameUIManager : MonoBehaviour
 
         GameManager.Instance.PlayerHealth.OnGameOver.RemoveListener(ShowGameOverPanel);
         GameManager.Instance.PlayerHealth.OnGameOver.AddListener(ShowGameOverPanel);
-
-        GameManager.Instance.LogInInit();
     }
 
     private void ResetScore(int newScore)
     {
-        inGameScoreText.text = newScore.ToString();
+        _inGameScoreText.text = newScore.ToString();
     }
 
     private void ShowGameOverPanel(int score)
     {
-        GameOverPanel.SetActive(true);
+        _gameOverPanel.SetActive(true);
         Time.timeScale = 0f;
 
         // 스코어 저장
-        gameOverScoreText.text = score.ToString();
+        _gameOverScoreText.text = score.ToString();
 
         int highScore = GameManager.Instance.GetRanking.HighScore;
-        highScoreText.text = highScore.ToString();
+        _highScoreText.text = highScore.ToString();
 
         if(score > highScore)
         {
-            newHighScoreText.SetActive(true);
+            _newHighScoreText.SetActive(true);
             GameManager.Instance.GetRanking.SetNewHighScore(score);
         }
     }
@@ -73,13 +71,13 @@ public class InGameUIManager : MonoBehaviour
     public void OnClickMenu()
     {
         Time.timeScale = 0f;
-        MenuPanel.SetActive(true);
+        _menuPanel.SetActive(true);
     }
 
     public void OnClickResume()
     {
         Time.timeScale = 1f;
-        MenuPanel.SetActive(false);
+        _menuPanel.SetActive(false);
     }
 
     public void OnClickExit()
